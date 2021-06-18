9f12570cec179a51a6c76ad712b3c5488d4a8b32
Natsukanai <n.mizuno777@gmail.com>
2021-06-19 00:51:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ AFRAME.registerComponent("update-distance", {
       Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2))
     );
     this.el.setAttribute("value", `${dl}m`);
+    console.log(v3);
   },
 });
 
