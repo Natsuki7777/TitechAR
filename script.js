eb27b0a2064f44c6b6d4b99b02dd884537542061
Natsukanai <n.mizuno777@gmail.com>
2021-07-23 17:35:03 +0900

    gps

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.addEventListener("load", () => {
-  let buildings = staticLoadbuildeings();
-  renderPlaces(buildings);
+  let models = staticLoadModels();
+  renderPlaces(models);
 });
 
 window.addEventListener("gps-camera-update-position", (e) => {
   console.log(e);
-  const distances = document.querySelectorAll(".distance");
+  const distances = document.querySelectorAll("distance");
   console.dir(distances);
   distances.forEach((distance) => {
     v3 = distance.getAttribute("position");
@@ -19,42 +19,46 @@ window.addEventListener("gps-camera-update-position", (e) => {
   });
 });
 
-function renderPlaces(buildings) {
+function renderPlaces(Models) {
   let scene = document.querySelector("a-scene");
 
-  buildings.forEach((building) => {
-    let building_name = building.name;
-    let latitude = building.location.lat;
-    let longitude = building.location.lng;
-    let building_height = building.height;
-    let model_type = building.model_type;
-    let caption = building.caption;
-    let open_time = building.open_time;
+  Models.forEach((Model) => {
+    let name = Model.name;
+    let latitude = Model.location.latitude;
+    let longitude = Model.location.longitude;
+    let height = Model.location.height;
+    let modelname = Model.model;
+    let caption = Model.caption;
+    let label = Model.label;
+    let minDistance = Model.minDistance;
+    let maxDistance = Model.maxDistance;
+    let distancelabel = Model.distance;
+    let link = Model.link;
     const scale = 60;
     // let scale = building.scale;
 
-    let model = document.createElement("a-entity");
-    model.setAttribute(
-      "gps-projected-entity-place",
+    let entity = document.createElement("a-entity");
+    entity.setAttribute(
+      "gps-entity-place",
       `latitude: ${latitude}; longitude: ${longitude};`
     );
-    model.setAttribute("position", { x: 0, y: building_height, z: 0 });
-    model.setAttribute(
+    entity.setAttribute("position", { x: 0, y: height, z: 0 });
+    entity.setAttribute(
       "gltf-model",
-      `./assets/model/${model_type}/${model_type}.gltf`
+      `./assets/model/${modelname}/${modelname}.gltf`
     );
-    model.setAttribute("animation-mixer", "");
-    model.addEventListener("loaded", () => {
+    entity.setAttribute("animation-mixer", "");
+    entity.addEventListener("loaded", () => {
       window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
     });
 
     //------------------文字--------------------------------
     let discription = document.createElement("a-text");
     discription.setAttribute(
-      "gps-projected-entity-place",
+      "gps-entity-place",
       `latitude: ${latitude}; longitude: ${longitude};`
     );
-    discription.setAttribute("position", { x: 10, y: building_height, z: 0 });
+    discription.setAttribute("position", { x: 10, y: height, z: 0 });
     discription.setAttribute("scale", {
       x: scale,
       y: scale,
@@ -62,7 +66,7 @@ function renderPlaces(buildings) {
     });
     discription.setAttribute("font", "./assets/font/custom-msdf.json");
     discription.setAttribute("font-image", "./assets/font/custom.png");
-    discription.setAttribute("value", building_name);
+    discription.setAttribute("value", name);
     discription.setAttribute("negate", false);
     discription.setAttribute("color", "black");
     discription.setAttribute("look-at", "[camera]");
@@ -70,13 +74,13 @@ function renderPlaces(buildings) {
     //------------------距離--------------------------------
     let distance = document.createElement("a-text");
     distance.setAttribute(
-      "gps-projected-entity-place",
+      "gps-entity-place",
       `latitude: ${latitude}; longitude: ${longitude};`
     );
     distance.setAttribute("class", "distance");
     distance.setAttribute("position", {
       x: 10,
-      y: building_height - 20,
+      y: height - 20,
       z: 0,
     });
     distance.setAttribute("scale", {
@@ -90,61 +94,77 @@ function renderPlaces(buildings) {
 
     console.dir(distance);
 
-    scene.appendChild(model);
+    scene.appendChild(entity);
     scene.appendChild(discription);
     scene.appendChild(distance);
   });
 }
 
-function staticLoadbuildeings() {
+function staticLoadModels() {
   return [
     {
       id: 1,
       name: "本館",
       location: {
-        lat: 35.60456954,
-        lng: 139.68385423,
+        latitude: 35.60456954,
+        longitude: 139.68385423,
+        height: 20,
       },
-      height: 20,
-      model_type: "pin",
+      model: "pin",
+      label: "",
+      minDistance: 0,
+      maxDistance: 0,
+      distance: true,
       caption: "",
-      open_time: "",
+      link: "",
     },
     {
       id: 2,
       name: "滝プラザ",
       location: {
-        lat: 35.60618984,
-        lng: 139.68464816,
+        latitude: 35.60618984,
+        longitude: 139.68464816,
+        height: 20,
       },
-      height: 20,
-      model_type: "pin",
+      model: "pin",
+      label: "",
+      minDistance: 0,
+      maxDistance: 0,
+      distance: true,
       caption: "",
-      open_time: "",
+      link: "",
     },
     {
       id: 3,
       name: "東京工業大学附属図書館",
       location: {
-        lat: 35.60644499,
-        lng: 139.68397225,
+        latitude: 35.60644499,
+        longitude: 139.68397225,
+        height: 20,
       },
-      height: 20,
-      model_type: "pin",
+      model: "pin",
+      label: "",
+      minDistance: 0,
+      maxDistance: 0,
+      distance: true,
       caption: "",
-      open_time: "",
+      link: "",
     },
     {
       id: 4,
       name: "百年記念館",
       location: {
-        lat: 35.6068287769,
-        lng: 139.68478654721,
+        latitude: 35.6068287769,
+        longitude: 139.68478654721,
+        height: 20,
       },
-      height: 20,
-      model_type: "pin",
+      model: "pin",
+      label: "",
+      minDistance: 0,
+      maxDistance: 0,
+      distance: true,
       caption: "",
-      open_time: "",
+      link: "",
     },
   ];
 }
